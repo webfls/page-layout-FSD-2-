3d7896a65b3ca7cccddd23ea8f7399d7b831fd43
Ann <ann26198905@gmail.com>
2020-12-10 23:22:58 +0300

    add plugin(not working)

---
+++ src/components/includes/item-quantity-dropdown/item-quantity-dropdown.js
@@ -0,0 +1,55 @@
+
+
+$(document).ready(function(){
+  $('.iqdropdown').iqDropdown({
+    // max total items
+    maxItems: Infinity,
+    // min total items
+    minItems: 0,
+    // text to show on the dropdown override data-selection-text attribute
+    selectionText: 'item',
+    // text to show for multiple items
+    textPlural: 'items',
+    // optionally can use setSelectionText function to override selectionText
+    setSelectionText: (itemCount, totalItems) => { /* return string */ },
+    // buttons to increment/decrement
+    controls: {
+      position: 'right',
+      displayCls: 'iqdropdown-item-display',
+      controlsCls: 'iqdropdown-item-controls',
+      counterCls: 'counter'
+    },
+    // fires when an item quantity changes
+    onChange: (id, count, totalItems) => {},
+    // return false to prevent an item decrement
+    beforeDecrement: (id, itemCount) => {},
+    // return false to prevent an item increment
+    beforeIncrement: (id, itemCount) => {}
+  })
+})
+
+// {
+//   // max total items
+//   maxItems: Infinity,
+//   // min total items
+//   minItems: 0,
+//   // text to show on the dropdown override data-selection-text attribute
+//   selectionText: 'item',
+//   // text to show for multiple items
+//   textPlural: 'items',
+//   // optionally can use setSelectionText function to override selectionText
+//   setSelectionText: (itemCount, totalItems) => { /* return string */ },
+//   // buttons to increment/decrement
+//   controls: {
+//     position: 'right',
+//     displayCls: 'iqdropdown-item-display',
+//     controlsCls: 'iqdropdown-item-controls',
+//     counterCls: 'counter'
+//   },
+//   // fires when an item quantity changes
+//   onChange: (id, count, totalItems) => {},
+//   // return false to prevent an item decrement
+//   beforeDecrement: (id, itemCount) => {},
+//   // return false to prevent an item increment
+//   beforeIncrement: (id, itemCount) => {}
+// }
